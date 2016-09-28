35bfdf310f53bc989a76acc58c188a01137d6858
Jay Ullman <jayullman@gmail.com>
2016-09-27 21:59:54 -0700

    WIP: make func createChannelDiv

--- twitch.js
+++ twitch.js
@@ -5,24 +5,27 @@ var channels = ["ESL_SC2", "OgamingSC2", "cretetion",
 function createChannelDiv(data) {
   var resultsContainer = document.getElementById("resultsContainer");
 
-  var channelDiv = document.createElement("div");
-  var channelTitle = document.createElement("h1");
-  var channelStatus = document.createElement("p");
+    var channelDiv = document.createElement("div");
+    var channelTitle = document.createElement("h1");
+    channelTitle.innerHTML = title;
 
-  channelDiv.setAttribute("class", "channelDiv");
-  channelDiv.appendChild(channelTitle);
-  channelDiv.appendChild(channelStatus);
+    var channelStatus = document.createElement("p");
 
-  resultsContainer.appendChild(channelDiv);
+    channelDiv.setAttribute("class", "channelDiv");
+    channelDiv.appendChild(channelTitle);
+    channelDiv.appendChild(channelStatus);
+
+    resultsContainer.appendChild(channelDiv);
 }
 
 $(document).ready(function() {
-
+// TODO: need a closure here to send channelname to callback
   for (var i = 0; i < channels.length; i++) {
     $.getJSON('https://api.twitch.tv/kraken/streams/' + channels[i] +
      '?client_id=ndpaljo31vlro8fjjdyvfr3o2t130nv&callback=?',
       function(data) {
       console.log(data);
+
 
     });
   }
