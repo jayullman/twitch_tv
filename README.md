2d1601cfd06f2d061d130e44e30f3224c09e3687
Jay Ullman <jayullman@gmail.com>
2016-09-28 07:36:54 -0700

    Create closure to capture channel names. Modify README.md

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 #twitch_tv
+
+Using jQuery for JSONP call to Twitch API
+
+Learned about closures because I needed a way to capture variables in a loop
+and send that over to the call back which would display the information
--- twitch.js
+++ twitch.js
@@ -2,12 +2,13 @@ var channels = ["ESL_SC2", "OgamingSC2", "cretetion",
 "freecodecamp", "storbeck", "habathcx", "RobotCaleb",
 "noobs2ninjas"];
 
-function createChannelDiv(data) {
-  var resultsContainer = document.getElementById("resultsContainer");
+function outer(channelName) {
+  function createChannelDiv(data) {
+    var resultsContainer = document.getElementById("resultsContainer");
 
     var channelDiv = document.createElement("div");
     var channelTitle = document.createElement("h1");
-    channelTitle.innerHTML = title;
+    channelTitle.innerHTML = channelName;
 
     var channelStatus = document.createElement("p");
 
@@ -16,18 +17,22 @@ function createChannelDiv(data) {
     channelDiv.appendChild(channelStatus);
 
     resultsContainer.appendChild(channelDiv);
+  }
+  return createChannelDiv;
 }
 
 $(document).ready(function() {
 // TODO: need a closure here to send channelname to callback
+
   for (var i = 0; i < channels.length; i++) {
+    var closureCall = outer(channels[i]);
     $.getJSON('https://api.twitch.tv/kraken/streams/' + channels[i] +
      '?client_id=ndpaljo31vlro8fjjdyvfr3o2t130nv&callback=?',
-      function(data) {
-      console.log(data);
+      closureCall
+      // console.log(data);
 
 
-    });
+    );
   }
 
 
