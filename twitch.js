7f7fc60524955dd03656760b998a10be9cbe5542
Jay Ullman <jayullman@gmail.com>
2016-09-28 07:42:39 -0700

    Rename function that creates closure as well of other functions

--- twitch.js
+++ twitch.js
@@ -2,7 +2,7 @@ var channels = ["ESL_SC2", "OgamingSC2", "cretetion",
 "freecodecamp", "storbeck", "habathcx", "RobotCaleb",
 "noobs2ninjas"];
 
-function outer(channelName) {
+function createFunction(channelName) {
   function createChannelDiv(data) {
     var resultsContainer = document.getElementById("resultsContainer");
 
@@ -22,19 +22,12 @@ function outer(channelName) {
 }
 
 $(document).ready(function() {
-// TODO: need a closure here to send channelname to callback
-
   for (var i = 0; i < channels.length; i++) {
-    var closureCall = outer(channels[i]);
+    var displayData = createFunction(channels[i]);
     $.getJSON('https://api.twitch.tv/kraken/streams/' + channels[i] +
      '?client_id=ndpaljo31vlro8fjjdyvfr3o2t130nv&callback=?',
-      closureCall
-      // console.log(data);
-
-
-    );
+      displayData);
   }
-
-
-
 });
+
+// TODO: renane closures and functions
