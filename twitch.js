339924cd45406936254f74656012ab8669f3e9fe
Jay Ullman <jayullman@gmail.com>
2016-10-01 15:00:01 -0700

    Update ajax callback function to create channel objects
    New function will create objects and push them onto the fullChannelList
    and either the onlineList or offlineList depending on their stream status.

--- twitch.js
+++ twitch.js
@@ -2,25 +2,26 @@
 Notes:
 - convert the channel divs into objects and store in an array
 - create function that builds list of results with the objects
+- add setInerval to refresh channel lists every so often
+- add 'Last Updated: <time>'
 */
 
 var channels = ["ESL_SC2", "OgamingSC2", "cretetion",
 "freecodecamp", "storbeck", "habathcx", "RobotCaleb",
 "noobs2ninjas", "comster404"];
+var fullChannelList = [];
+var onlineList = []; // collect divs that meet search criteria
+var offlineList = []; // collect divs that meet search criteria
 
 function updateChannels(e) {
 
-  var fullChannelList =  [];
   var resultsContainer = document.getElementById('resultsContainer');
-
-  var filteredChannelList = []; // collect divs that meet search criteria
 
   function clearResults() {
     if (resultsContainer.hasChildNodes()) {
       while (resultsContainer.hasChildNodes()) {
         resultsContainer.removeChild(resultsContainer.firstChild)
       }
-
     }
   }
 
@@ -31,18 +32,13 @@ function updateChannels(e) {
     }
   }
 
-  fullChannelList = resultsContainer.getElementsByTagName('div');
-  console.log(fullChannelList);
-
   // following code runs only if updateChannels is called via one
   // of the 3 filtering buttons
   if (e != undefined) {
     switch (e.target.id) {
       case 'allButton':
       clearResults();
-      buildResults(fullChannelList);
       console.log(fullChannelList);
-
       break;
 
       case 'onlineButton':
@@ -58,38 +54,47 @@ function updateChannels(e) {
 
 }
 
-
-
-
-
+// TODO: change name of createFunction()
 function createFunction(channelName) {
-  function createChannelDiv(data) {
-    var resultsContainer = document.getElementById("resultsContainer");
 
-    var channelDiv = document.createElement("div");
-    var channelTitle = document.createElement("h1");
-    channelTitle.innerHTML = channelName;
-
-    var channelStatus = document.createElement("p");
-    if (data.hasOwnProperty("error")) {
-      channelStatus.innerHTML = "Account deactivated";
-    } else if (data.stream == null) {
-      channelStatus.innerHTML = "Not currently streaming";
-    } else {
-      channelStatus.innerHTML = "Currently playing " + data.stream.game;
+  function createLists(data) {
+    function Channel() {
+      this.channelName = channelName;
+      this.isPlaying = (data.stream == null) ? false : true;
+      this.accountActive = (data.error == 'Not Found') ? false : true;
+      this.URLToAccountPage = 'url';
     }
 
-
-    channelDiv.setAttribute("class", "channelDiv");
-    channelDiv.appendChild(channelTitle);
-    channelDiv.appendChild(channelStatus);
-
-    resultsContainer.appendChild(channelDiv);
-    updateChannels(); // remove: test
-
+    // var resultsContainer = document.getElementById("resultsContainer");
+    //
+    // var channelDiv = document.createElement("div");
+    // var channelTitle = document.createElement("h1");
+    // channelTitle.innerHTML = channelName;
+    //
+    // var channelStatus = document.createElement("p");
+    // if (data.hasOwnProperty("error")) {
+    //   channelStatus.innerHTML = "Account deactivated";
+    // } else if (data.stream == null) {
+    //   channelStatus.innerHTML = "Not currently streaming";
+    // } else {
+    //   channelStatus.innerHTML = "Currently playing " + data.stream.game;
+    // }
+    //
+    //
+    // channelDiv.setAttribute("class", "channelDiv");
+    // channelDiv.appendChild(channelTitle);
+    // channelDiv.appendChild(channelStatus);
+    //
+    // resultsContainer.appendChild(channelDiv);
+    var channelObj = new Channel();
+    fullChannelList.push(channelObj);
+    if (channelObj.isPlaying) {
+      onlineList.push(channelObj);
+    } else {
+      offlineList.push(channelObj);
+    }
   }
-
-  return createChannelDiv;
+  return createLists;
 }
 
 $(document).ready(function() {
@@ -100,9 +105,9 @@ $(document).ready(function() {
   document.getElementById('offlineButton').addEventListener('click', updateChannels);
 
   for (var i = 0; i < channels.length; i++) {
-    var displayData = createFunction(channels[i]);
+    var createChannelLists = createFunction(channels[i]);
     $.getJSON('https://api.twitch.tv/kraken/streams/' + channels[i] +
      '?client_id=ndpaljo31vlro8fjjdyvfr3o2t130nv&callback=?',
-      displayData);
+      createChannelLists);
   }
 });
