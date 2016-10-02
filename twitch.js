24c570b33a2ca9cb349d5f551bf3f8fd50b82cf7
Jay Ullman <jayullman@gmail.com>
2016-10-01 17:12:32 -0700

    Add ability to filter results by typing in text field

--- twitch.js
+++ twitch.js
@@ -12,46 +12,94 @@ var channels = ["ESL_SC2", "OgamingSC2", "cretetion",
 var fullChannelList = [];
 var onlineList = [];
 var offlineList = [];
-var filteredList = [] // filtered down list as user types into textfield
+var filteredList = []; // filtered down list as user types into textfield
+var currentView = 'all'; // setting to determine which view option user selected
 
-function updateChannels(e) {
-  var resultsContainer = document.getElementById('resultsContainer');
+function buildResults() {
+  // tests there is user input in the filter text box
+  // if there is, the list is built with the filteredList array
+  clearResults();
+  switch (currentView) {
+    case 'all':
+      list = fullChannelList;
+      break;
 
-  function clearResults() {
-    if (resultsContainer.hasChildNodes()) {
-      while (resultsContainer.hasChildNodes()) {
-        resultsContainer.removeChild(resultsContainer.firstChild);
-      }
-    }
+    case 'online':
+      list = onlineList;
+      break;
+
+    case 'offline':
+      list = offlineList;
+      break;
   }
 
-  function buildResults(list) {
-    var resultsContainer = document.getElementById("resultsContainer");
-    for (var i = 0; i < list.length; i++) {
-      var channelDiv = document.createElement("div");
-      var channelTitle = document.createElement("h1");
-      channelTitle.innerHTML = list[i].channelName;
-      var channelStatus = document.createElement("p");
+  if (document.getElementById('textFilterChannels').value != '') {
+    list = filteredList;
+  }
 
-      console.log(list[i]);
-      if (list[i].accountActive == false) {
-        channelStatus.innerHTML = "Account Deactivated";
-      } else if (list[i].isPlaying == true) {
-        channelStatus.innerHTML = "Currently playing " + list[i].game;
-      } else {
-        channelStatus.innerHTML = "Not currently streaming";
-      }
+  var resultsContainer = document.getElementById("resultsContainer");
+  for (var i = 0; i < list.length; i++) {
+    if (currentView == 'all' ||
+       (currentView == 'online' && list[i].isPlaying == true) ||
+       (currentView == 'offline' && list[i].isPlaying == false)
+     ) {
+       var channelDiv = document.createElement("div");
+       var channelTitle = document.createElement("h1");
+       channelTitle.innerHTML = list[i].channelName;
+       var channelStatus = document.createElement("p");
 
-      channelDiv.setAttribute("class", "channelDiv");
-      channelDiv.appendChild(channelTitle);
-      channelDiv.appendChild(channelStatus);
-      resultsContainer.appendChild(channelDiv);
+       if (list[i].accountActive == false) {
+         channelStatus.innerHTML = "Account Deactivated";
+       } else if (list[i].isPlaying == true) {
+         channelStatus.innerHTML = "Currently playing " + list[i].game;
+       } else {
+         channelStatus.innerHTML = "Not currently streaming";
+       }
+
+       channelDiv.setAttribute("class", "channelDiv");
+       channelDiv.appendChild(channelTitle);
+       channelDiv.appendChild(channelStatus);
+       resultsContainer.appendChild(channelDiv);
+     }
+  }
+}
+
+function typeFilter(e) {
+  var userInput = e.target.value.toLowerCase();
+
+  if (currentView == 'all') {
+    filteredList = fullChannelList.filter(function(t) {
+      // returns true if userInput is found within the channelName
+      return (t.channelName.toLowerCase().indexOf(userInput) > -1);
+    });
+  } else if (currentView == "online") {
+    onlineList = onlineChannelList.filter(function(t) {
+      return (t.channelName.toLowerCase().indexOf(userInput) > -1);
+    });
+  } else {
+    offlineList = offlineChannelList.filter(function(t) {
+      // returns true if userInput is found within the channelName
+      return (t.channelName.toLowerCase().indexOf(userInput) > -1);
+    });
+  }
+
+  buildResults(filteredList);
+}
+
+function clearResults() {
+  var resultsContainer = document.getElementById('resultsContainer');
+
+  if (resultsContainer.hasChildNodes()) {
+    while (resultsContainer.hasChildNodes()) {
+      resultsContainer.removeChild(resultsContainer.firstChild);
     }
-    }
+  }
+}
+
+function updateChannels(e) {
 
   // following code runs only if updateChannels is called via one
   // of the 3 filtering buttons
-
 
   // tests for array - this code runs on initial page loading
   if (e.length) {
@@ -59,17 +107,17 @@ function updateChannels(e) {
   } else if (e != undefined) {
     switch (e.target.id) {
       case 'allButton':
-      clearResults();
+      currentView = 'all';
       buildResults(fullChannelList);
       break;
 
       case 'onlineButton':
-      clearResults();
+      currentView = 'online';
       buildResults(onlineList);
       break;
 
       case 'offlineButton':
-      clearResults();
+      currentView = 'offline';
       buildResults(offlineList);
       break;
     }
@@ -112,6 +160,8 @@ $(document).ready(function() {
   document.getElementById('allButton').addEventListener('click', updateChannels);
   document.getElementById('onlineButton').addEventListener('click', updateChannels);
   document.getElementById('offlineButton').addEventListener('click', updateChannels);
+  document.getElementById('textFilterChannels').addEventListener('keyup', typeFilter);
+
 
   for (var i = 0; i < channels.length; i++) {
     var createChannelLists = createFunction(channels[i]);
